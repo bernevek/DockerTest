90e66d6045e696cbd04c67fefe4a23f92f1db923
bernevek <bernevek1995@ukr.net>
2019-07-10 09:15:18 +0300

    1st commit

---
+++ EmployeeDashBoardService/src/main/java/com/employeedashboardservice/controller/FeignEmployeeInfoController.java
@@ -0,0 +1,23 @@
+package com.employeedashboardservice.controller;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.employeedashboardservice.domain.model.Employee;
+@RefreshScope
+@RestController
+public class FeignEmployeeInfoController {
+    @Autowired
+    EmployeeServiceProxy employeeServiceProxy;
+    @RequestMapping("/dashboard/feign/{myself}")
+    public Employee findme(@PathVariable Long myself) {
+        return employeeServiceProxy.findById(myself);
+    }
+    @RequestMapping("/dashboard/feign/peers")
+    public Collection < Employee> findPeers() {
+        return employeeServiceProxy.findAll();
+    }
+}
